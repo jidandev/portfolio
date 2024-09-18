cfcbb963034564027db22fc64a3323a318faf344
jidandev <jidangamer7@gmail.com>
2024-09-18 05:52:55 +0000

    added some changes

--- script.js
+++ script.js
@@ -8,6 +8,78 @@ const aboutNav = document.querySelector('.aboutnav');
 const projectNav = document.querySelector('.projectnav');
 const skilsNav = document.querySelector('.skilsnav');
 const contactNav = document.querySelector('.contactnav');
+const tema = document.querySelector('.tema');
+let temaGelap = true;
+//TEMA
+tema.addEventListener('click', () => {
+    if(temaGelap) {
+        tema.innerHTML = `<ion-icon name="moon-outline"></ion-icon>`;
+        tema.style.color = `var(--text-color-light)`;
+        document.body.style.backgroundColor = `var(--primary-color-light)`;
+        document.querySelector('nav ul').style.backgroundColor = `var(--secondary-color-light)`;
+        for(let i = 0;i<5;i++) {
+            document.querySelectorAll('nav ul li')[i].style.backgroundColor = `var(--accent-color-light)`;
+            document.querySelectorAll('nav ul li')[i].style.color = `var(--text-color-light)`;
+        }
+        document.querySelector('.home h1').style.color = `var(--text-color-light)`;
+        document.querySelector('.home h1').style.borderColor = `var(--text-color-light)`;
+        document.querySelector('.home p').style.color = `var(--text-color-light)`;
+        document.querySelector('.about div').style.backgroundColor = `var(--secondary-color-light)`;
+        document.querySelector('.about div div').style.backgroundColor = `var(--accent-color-light)`;
+        document.querySelector('.about div h1').style.color = `var(--text-color-light)`;
+        document.querySelector('.about div h1').style.borderColor = `var(--text-color-light)`;
+        document.querySelector('.about div p').style.color = `var(--text-color-light)`;
+        document.querySelector('.about section h1').style.color = `var(--text-color-light)`;
+        document.querySelector('.about section h1').style.borderColor = `var(--text-color-light)`;
+        document.querySelector('.about section p').style.color = `var(--text-color-light)`;
+        document.querySelector('.project h1').style.color = `var(--text-color-light)`;
+        document.querySelector('.project h1').style.borderColor = `var(--text-color-light)`;
+        document.querySelector('.project ul li').style.backgroundColor = `var(--secondary-color-light)`;
+        document.querySelector('.project ul li h1').style.color = `var(--text-color-light)`;
+        document.querySelector('.project ul li h1').style.borderColor = `var(--text-color-light)`;
+        document.querySelector('.project ul li a').style.color = `var(--text-color-light)`;
+        document.querySelector('.project ul li .github').style.color = `var(--text-color-light)`;
+        document.querySelector('.skils h1').style.color = `var(--text-color-light)`;
+        document.querySelector('.skils h1').style.borderColor = `var(--text-color-light)`;
+        document.querySelector('.contact h1').style.color = `var(--text-color-light)`;
+        document.querySelector('.contact h1').style.borderColor = `var(--text-color-light)`;
+        document.querySelector('.githubimg').style.filter = `invert(0%)`;
+        temaGelap = false;
+    } else {
+        tema.innerHTML = `<ion-icon name="sunny-outline"></ion-icon>`
+        tema.style.color = `var(--text-color)`;
+        document.body.style.backgroundColor = `var(--primary-color)`;
+        document.querySelector('nav ul').style.backgroundColor = `var(--secondary-color)`;
+        for(let i = 0;i<5;i++) {
+            document.querySelectorAll('nav ul li')[i].style.backgroundColor = `var(--accent-color)`;
+            document.querySelectorAll('nav ul li')[i].style.color = `var(--text-color)`;
+        }
+        document.querySelector('.home h1').style.color = `var(--text-color)`;
+        document.querySelector('.home h1').style.borderColor = `var(--text-color)`;
+        document.querySelector('.home p').style.color = `var(--text-color)`;
+        document.querySelector('.about div').style.backgroundColor = `var(--secondary-color)`;
+        document.querySelector('.about div div').style.backgroundColor = `var(--accent-color)`;
+        document.querySelector('.about div h1').style.color = `var(--text-color)`;
+        document.querySelector('.about div h1').style.borderColor = `var(--text-color)`;
+        document.querySelector('.about div p').style.color = `var(--text-color)`;
+        document.querySelector('.about section h1').style.color = `var(--text-color)`;
+        document.querySelector('.about section h1').style.borderColor = `var(--text-color)`;
+        document.querySelector('.about section p').style.color = `var(--text-color)`;
+        document.querySelector('.project h1').style.color = `var(--text-color)`;
+        document.querySelector('.project h1').style.borderColor = `var(--text-color)`;
+        document.querySelector('.project ul li').style.backgroundColor = `var(--secondary-color)`;
+        document.querySelector('.project ul li h1').style.color = `var(--text-color)`;
+        document.querySelector('.project ul li h1').style.borderColor = `var(--text-color)`;
+        document.querySelector('.project ul li a').style.color = `var(--text-color)`;
+        document.querySelector('.project ul li .github').style.color = `var(--text-color)`;
+        document.querySelector('.skils h1').style.color = `var(--text-color)`;
+        document.querySelector('.skils h1').style.borderColor = `var(--text-color)`;
+        document.querySelector('.contact h1').style.color = `var(--text-color)`;
+        document.querySelector('.contact h1').style.borderColor = `var(--text-color)`;
+        document.querySelector('.githubimg').style.filter = `invert(100%)`;
+        temaGelap = true;
+    }
+})
 
 //NAV
 function smoothScroll(event, targetId) {
